3405830ed8b2a04d2a0df366f18c41fe7edfb50e
speed <43330808+speedxx@users.noreply.github.com>
2020-04-08 13:41:49 -0400

    version 2.5

--- commands/miscellaneous/devs.js
+++ commands/miscellaneous/devs.js
@@ -1,20 +1,23 @@
 const Discord = require("discord.js")
 function createprompt(icon, title, content) {
-    prompt = "http://atom.smasher.org/error/98.png.php?icon="+ icon +"&style=98&title=" + title + "&url=&text=" + content  + "&.png"
+    prompt = "http://atom.smasher.org/error/98.png.php?icon=" + icon + "&style=98&title=" + title + "&url=&text=" + content
     return prompt
 }
 module.exports = {
-config: {
-    name: "devs",
-    aliases: ["developers"],
-    usage: "win98 devs",
-    description: "provides information about the developers",
-    noalias: "no aliases",
-    accessability: "everyone"
-},
-run: (client, message, args) => {
-    message.channel.send({
-        files: [createprompt("hwinfo", "Developers", "The+developers+of+Win98+are%3A%0D%0Asquare%231255%2C+speed%231255+and+fionn%234512.%0D%0AThe+Windows+98+prompts+are+created+via+atomsmasher.")]
-    })
+    config: {
+        name: "devs",
+        aliases: ["developers"],
+        usage: "win98 devs",
+        description: "provides information about the developers",
+        noalias: "no aliases",
+        accessability: "everyone"
+    },
+    run: (client, message, args) => {
+        message.channel.send({
+            files: [{
+                attachment: createprompt("hwinfo", "Developers", "The+developers+of+Win98+are%3A%0D%0Asquare%231255%2C+speed%231255+and+fionn%234512.%0D%0AThe+Windows+98+prompts+are+created+via+atomsmasher."),
+                name: "Devs.png"
+            }]
+        })
     }
 }
