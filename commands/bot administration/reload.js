cef3c0cf1001deafebe4c8cb5936db4b931fa2bd
speed <43330808+speedxx@users.noreply.github.com>
2020-04-08 14:58:35 -0400

    version 2.5

--- commands/bot administration/reload.js
+++ commands/bot administration/reload.js
@@ -1,45 +1,63 @@
 const { Discord } = require("discord.js")
+function createprompt(icon, title, content) {
+  prompt = "http://atom.smasher.org/error/98.png.php?icon=" + icon + "&style=98&title=" + title + "&url=&text=" + content
+  return prompt
+}
 
 module.exports = {
   config: {
-      name: "reload",
-      aliases: ["r", "reload"],
-      usage: "win98 reload [command name]",
-      description: "reloads command provided",
-      noalias: "no aliases",
-      accessability: "bot administrators"
+    name: "reload",
+    aliases: ["r", "reload"],
+    usage: "win98 reload [command name]",
+    description: "reloads command provided",
+    noalias: "no aliases",
+    accessability: "bot administrators"
   },
   run: (client, message, args) => {
 
-    if(message.author.id != "372078453236957185") { 
-      if(message.author.id != "365274392680333329") {
-        if(message.author.id != "697050554383335484") {
-        return message.channel.send({
-          files: ["./prompts/permissions failure.png"]
-        })
+    if (message.author.id != "372078453236957185") {
+      if (message.author.id != "365274392680333329") {
+        if (message.author.id != "697050554383335484") {
+          return message.channel.send({
+            files: [{
+              attachment: createprompt("keys", "Error", "Insufficient+permissions."),
+              name: "Permissions failure.png"
+            }]
+          })
+        }
       }
-    }}
-  if(!args || args.length < 1) return message.reply("Command name not provided.", {
-    files:["./prompts/arguments failure.png"]
-  });
-  
-  let commandName = args[0].toLowerCase()
+    }
+    if (!args || args.length < 1) return message.channel.send({
+      files: [{
+        attachment: createprompt("disk_red_lbl", "Error", "Insufficient+arguments+added+to+command."),
+        name: "Arguments failure.png"
+      }]
+    })
 
-  try {
-   delete require.cache[require.resolve(`./${commandName}.js`)]
+    let commandName = args[0].toLowerCase()
 
-   client.commands.delete(commandName)
-   const pull = require(`./${commandName}.js`)
-   client.commands.set(commandName, pull)
-   
- } catch(e) {
-  return message.channel.send(`Windows 98 failed to reload ${args[0].toUpperCase()} because ${e.message}`, {
-    files: ["./prompts/reload failure.png"]
-  })
- }
+    try {
+      delete require.cache[require.resolve(`./${commandName}.js`)]
 
- message.channel.send(`Windows 98 reloaded ${args[0].toUpperCase()}`, {
-   files: ["./prompts/reload success.png"]
-  })
-}
+      client.commands.delete(commandName)
+      const pull = require(`./${commandName}.js`)
+      client.commands.set(commandName, pull)
+
+    } catch (e) {
+      console.log(e.stack)
+      return message.channel.send({
+        files: [{
+          attachment: createprompt("Error", "Error", "Failed+to+reload+" + args[0]),
+          name: "Reload failure.png"
+        }]
+      })
+    }
+
+    message.channel.send({
+      files: [{
+        attachment: createprompt("bubble_i", "Success", "Successfully+reloaded+" + args[0]),
+        name: "Reload success.png"
+      }]
+    })
+  }
 }
