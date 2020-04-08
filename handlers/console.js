039ed14b4df8b1212af21c6dd557af9c1776ce11
speed <43330808+speedxx@users.noreply.github.com>
2020-04-08 15:29:31 -0400

    version 2.5

--- handlers/console.js
+++ handlers/console.js
@@ -1,12 +1,19 @@
+function createprompt(icon, title, content) {
+    prompt = "http://atom.smasher.org/error/98.png.php?icon=" + icon + "&style=98&title=" + title + "&url=&text=" + content
+    return prompt
+}
+
 module.exports = (client) => {
     let prom = process.openStdin()
     prom.addListener("data", res => {
         let x = res.toString().trim().split(/ +/g)
         let args = x[0]
         let z = res.toString().trim().split(/ +/g).slice(1)
-        client.channels.cache.get(args).send(z.join(" "), {
-            files: ["./prompts/remote access.png"]
-
+        client.channels.cache.get(args).send({
+            files: [{
+                attachment: createprompt("ups", "Remote Access", z),
+                name: "Remote access.png"
+            }]
         })
         console.log(`Successfully sent '${z}' to ${args}`);
     })
