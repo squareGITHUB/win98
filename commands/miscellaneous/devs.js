f1293044783bc8aad5bb2258a61ed4d84d9dfcc4
squareGITHUB <44121785+squareGITHUB@users.noreply.github.com>
2020-04-08 18:14:26 +0100

    version 2.1.0

--- commands/miscellaneous/devs.js
+++ commands/miscellaneous/devs.js
@@ -1,5 +1,8 @@
 const Discord = require("discord.js")
-
+function createprompt(icon, title, content) {
+    prompt = "http://atom.smasher.org/error/98.png.php?icon="+ icon +"&style=98&title=" + title + "&url=&text=" + content  + "&.png"
+    return prompt
+}
 module.exports = {
 config: {
     name: "devs",
@@ -11,7 +14,7 @@ config: {
 },
 run: (client, message, args) => {
     message.channel.send({
-        files: ["./prompts/developers.png"]
+        files: [createprompt("hwinfo", "Developers", "The+developers+of+Win98+are%3A%0D%0Asquare%231255%2C+speed%231255+and+fionn%234512.%0D%0AThe+Windows+98+prompts+are+created+via+atomsmasher.")]
     })
     }
 }
