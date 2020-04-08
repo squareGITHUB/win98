621c4ce017fd06d68c8a121ef5f04925e7311ef0
speed <43330808+speedxx@users.noreply.github.com>
2020-04-08 15:00:15 -0400

    version 2.5

--- commands/bot administration/shutdown.js
+++ commands/bot administration/shutdown.js
@@ -1,31 +1,40 @@
-const { Discord } = require("discord.js")
+const { Discord } = require("discord.js");
+function createprompt(icon, title, content) {
+  prompt = "http://atom.smasher.org/error/98.png.php?icon=" + icon + "&style=98&title=" + title + "&url=&text=" + content
+  return prompt
+}
 
 module.exports = {
   config: {
-      name: "shutdown",
-      aliases: ["sd", "shutdown"],
-      usage: "win98 shutdown",
-      description: "shuts prompt down",
-      accessability: "bot administrators"
+    name: "shutdown",
+    aliases: ["sd", "shutdown"],
+    usage: "win98 shutdown",
+    description: "shuts prompt down",
+    accessability: "bot administrators"
   },
   run: async (client, message, args) => {
-    if(message.author.id != "372078453236957185") { 
-      if(message.author.id != "365274392680333329") {
-        if(message.author.id != "697050554383335484") {
-        return message.channel.send({
-          files: ["./prompts/permissions failure.png"]
-        })
+    if (message.author.id != "372078453236957185") {
+      if (message.author.id != "365274392680333329") {
+        if (message.author.id != "697050554383335484") {
+          return message.channel.send({
+            files: [{
+              attachment: createprompt("keys", "Error", "Insufficient+permissions."),
+              name: "Permissions failure.png"
+            }]
+          })
+        }
       }
-    }}
+    }
     try {
-    await message.channel.send({
-      files: ['./prompts/shutdown.png']
-    })
-    process.exit()
+      await message.channel.send({
+        files: [{
+          attachment: createprompt("skull3", "Shutdown", "Windows+98+is+shutting+down..."),
+          name: "Shutdown.png"
+        }]
+      })
+      process.exit()
     } catch (e) {
-        return message.channel.send(`Windows 98 failed to shutdown because ${e.message}\``, {
-          files: ["./prompts/shutdown failure.png"]
-        })
+      console.log(e.stack)
     }
   }
 }
