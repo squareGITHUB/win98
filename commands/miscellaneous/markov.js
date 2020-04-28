d2b414d904f2e4f5075d1729b487230655ec7485
speed <43330808+speedxx@users.noreply.github.com>
2020-04-28 15:26:17 -0400

    Add files via upload

---
+++ commands/miscellaneous/markov.js
@@ -0,0 +1,46 @@
+const Discord = require("discord.js")
+function createprompt(icon, title, content) {
+    prompt = "http://atom.smasher.org/error/98.png.php?icon=" + icon + "&style=98&title=" + title + "&url=&text=" + content
+    return prompt
+}
+
+module.exports = {
+    config: {
+        name: "markov",
+        aliases: [],
+        usage: "win98 markov [text]",
+        description: "generates a markov chain",
+        noalias: "no aliases",
+        accessability: "everyone"
+    },
+    run: (client, message, args) => {
+
+        let text = args.splice(' ')
+        let markovChain = {};
+        for (let i = 0; i < text.length; i++) {
+            let word = text[i].replace(/[\W_]/, "")
+            if (!markovChain[word]) {
+                markovChain[word] = []
+            }
+            if (text[i + 1]) {
+                markovChain[word].push(text[i + 1].replace(/[\W_]/, ""));
+            }
+        }
+
+        const words = Object.keys(markovChain)
+        let word = words[Math.floor(Math.random() * words.length)]
+        let result = ''
+        for (let i = 0; i < words.length; i++) {
+            result += word + ' ';
+            newWord = markovChain[word][Math.floor(Math.random() * markovChain[word].length)]
+            word = newWord;
+            if (!word || !markovChain.hasOwnProperty(word)) word = words[Math.floor(Math.random() * words.length)]
+        }
+        return message.channel.send({
+            files: [{
+                attachment: createprompt("mail", "Markov+Chain+(" + message.author.username + ")", result),
+                name: "Markov.png"
+            }]
+        })
+    }
+}
